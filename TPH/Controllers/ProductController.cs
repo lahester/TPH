3e675639b17a6eaf5a7dfa7989e6ced8dbd57343
Brady Pratt <29494941+jbpratt78@users.noreply.github.com>
2018-04-09 20:11:40 -0500

    Working form

--- TPH/Controllers/ProductController.cs
+++ TPH/Controllers/ProductController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TPH.Models;
+using TPH.ViewModels;
+using System.Collections;
+using System.Data.Entity;
 
 namespace TPH.Controllers
 {
@@ -18,6 +21,42 @@ namespace TPH.Controllers
         public ActionResult Form()
         {
             return View();
+        }
+
+        private ApplicationDbContext _context;
+        public ProductController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+
+        public ActionResult New()
+        {
+            var viewModel = new ProductFormViewModel
+            {
+                Product = new Product()
+            };
+            return View("Form", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new ProductFormViewModel
+                {
+                    Product = product
+                };
+                return View("Form", viewModel);
+            }
+            else
+            {
+                _context.Products.Add(product);
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Product");
         }
     }
 }
---
+++ TPH/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPH.Models;
+
+namespace TPH.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public Product Product { get; set; }
+    }
+}
