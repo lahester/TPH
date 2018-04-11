422e7e3f1c02d772756ff4365ea683929170d237
Brady Pratt <29494941+jbpratt78@users.noreply.github.com>
2018-04-11 16:41:43 -0700

    Displaying products

---
+++ TPH/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPH.Models;
+
+namespace TPH.ViewModels
+{
+    public class ProductsViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
